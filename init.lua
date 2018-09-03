5e088537f4bc79bfe9106e9d0cf51b38c8e0efec
Thomas Rudin <thomas@rudin.io>
2018-09-03 11:17:00 +0200

    grant priv step wip

--- init.lua
+++ init.lua
@@ -56,6 +56,7 @@ dofile(MP.."/steps/chestreward.lua")
 dofile(MP.."/steps/givereward.lua")
 dofile(MP.."/steps/message.lua")
 dofile(MP.."/steps/teleport.lua")
+dofile(MP.."/steps/grant.lua")
 -- dofile(MP.."/steps/followup.lua")
 
 if has_xp_redo_mod then
